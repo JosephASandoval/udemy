de86870b20f0b212512890ed74d155ff85b2c6d9
JosephASandoval <josephasandoval17@gmail.com>
2021-10-05 15:23:24 -0700

    updated

--- algo.js
+++ algo.js
@@ -216,34 +216,52 @@
 // let arr = [2, 5, 1, 46, 23];
 // console.log(insertionSort(arr)); // [1, 2, 5, 23, 46]
 
-function mergeSort(arr) {
-  if (arr.length === 1) return arr;
+// function mergeSort(arr) {
+//   if (arr.length <= 1) return arr;
 
-  const midIdx = Math.floor(arr.length / 2);
-  const left = arr.slice(0, midIdx);
-  const right = arr.slice(midIdx);
+//   const midIdx = Math.floor(arr.length / 2);
+//   const left = arr.slice(0, midIdx);
+//   const right = arr.slice(midIdx);
 
-  const sortedLeft = mergeSort(left);
-  const sortedRight = mergeSort(right);
+//   const sortedLeft = mergeSort(left);
+//   const sortedRight = mergeSort(right);
 
-  return merge(sortedLeft, sortedRight);
+//   return merge(sortedLeft, sortedRight);
+// }
+
+// function merge(sortedLeft, sortedRight) {
+//   const mergedArr = [];
+
+//   while (sortedLeft.length > 0 && sortedRight.length > 0) {
+//     if (sortedLeft[0] < sortedRight[0]) {
+//       mergedArr.push(sortedLeft.shift())
+//     } else if (sortedRight[0] < sortedLeft[0]) {
+//       mergedArr.push(sortedRight.shift())
+//     } else {
+//       mergedArr.push(sortedRight.shift());
+//     }
+//   }
+
+//   return mergedArr.concat(sortedLeft, sortedRight);
+// }
+
+// console.log(mergeSort([1, 5, 24, 76, 1, 3]))
+
+// function quickSort(arr) {
+//   if (arr.length <= 1) return arr;
+
+//   let pivot = arr[0];
+//   let left = arr.slice(1).filter((el) => el < pivot);
+//   let right = arr.slice(1).filter((el) => el >= pivot);
+
+//   let sortedLeft = quickSort(left);
+//   let sortedRight = quickSort(right);
+
+//   return sortedLeft.concat([pivot], sortedRight);
+// }
+
+// console.log(quickSort([1, 5, 24, 76, 1, 3]));
+
+function radixSort() {
+  
 }
-
-function merge(sortedLeft, sortedRight) {
-  const mergedArr = [];
-
-  while (sortedLeft.length > 0 && sortedRight.length > 0) {
-    if (sortedLeft[0] < sortedRight[0]) {
-      mergedArr.push(sortedLeft.shift())
-    } else if (sortedRight[0] < sortedLeft[0]) {
-      mergedArr.push(sortedRight.shift())
-    } else {
-      mergedArr.push(sortedRight.shift());
-    }
-  }
-
-  return mergedArr.concat(sortedLeft, sortedRight);
-}
-
-console.log(mergeSort([1, 5, 24, 76, 1, 3]))
-
