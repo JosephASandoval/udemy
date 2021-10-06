cb2e49c281ced512f9a40d322f94faf5db9e68a8
JosephASandoval <josephasandoval17@gmail.com>
2021-10-06 09:17:12 -0700

    udpdated

---
+++ data_structures/practice.js
@@ -0,0 +1,64 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return null;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+
+  print() {
+    let arr = [];
+    let current = this.head;
+    while (current) {
+      arr.push(current.value);
+      current = current.next;
+    }
+    console.log(arr);
+  }
+}
+
+let list = new SinglyLinkedList();
+
+list.push("hi");
+list.push("bye");
+console.log(list.pop())
+// list.pop();
+// list.print();
+// console.log(list.length);
--- data_structures/singly_linked_list.js
+++ data_structures/singly_linked_list.js
@@ -144,3 +144,5 @@ list.push(1);
 list.push(2);
 list.push(3);
 list.push(4);
+
+list.print();
