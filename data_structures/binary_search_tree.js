7a640c625904aa6e02360a9c57bcdd952fed2df4
JosephASandoval <josephasandoval17@gmail.com>
2021-10-12 10:38:57 -0700

    updated

--- data_structures/binary_search_tree.js
+++ data_structures/binary_search_tree.js
@@ -11,7 +11,44 @@ class BinarySearchTree {
     this.root = null;
   }
 
-  insert(value) {}
+  insert(value) {
+    const newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    const current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
 }
 
+//      10
+//   5     13
+// 2  7  11  16
+
 const tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
+
+console.log(tree);
