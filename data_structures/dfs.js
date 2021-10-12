0760e62942787a8fed4cf0db166c5eb2428654f0
JosephASandoval <josephasandoval17@gmail.com>
2021-10-12 13:27:39 -0700

    updated

--- data_structures/dfs.js
+++ data_structures/dfs.js
@@ -54,18 +54,36 @@ class BinarySearchTree {
     return false;
   }
 
-  BFS() {
-    let node = this.root;
-    const data = [];
-    const queue = [];
-    queue.push(node);
-
-    while (queue.length) {
-      node = queue.shift();
+  DFSPreOrder() {
+    let data = [];
+    function traverse(node) {
       data.push(node.value);
-      if (node.left) queue.push(node.left);
-      if (node.right) queue.push(node.right);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
     }
+    traverse(this.root);
+    return data;
+  }
+
+  DFSPostOrder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    traverse(this.root);
+    return data;
+  }
+
+  DFSInOrder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
     return data;
   }
 }
@@ -82,4 +100,6 @@ tree.insert(11);
 tree.insert(2);
 tree.insert(16);
 tree.insert(7);
-console.log(tree.BFS());
+console.log(tree.DFSPreOrder());
+console.log(tree.DFSPostOrder());
+console.log(tree.DFSInOrder());
