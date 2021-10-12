e704ee103c88a764547a2fd54a59666631e3c180
JosephASandoval <josephasandoval17@gmail.com>
2021-10-12 13:10:30 -0700

    updated

--- data_structures/bfs.js
+++ data_structures/bfs.js
@@ -76,9 +76,10 @@ class BinarySearchTree {
 
 let tree = new BinarySearchTree();
 tree.insert(10);
-tree.insert(6);
-tree.insert(15);
-tree.insert(3);
-tree.insert(8);
-tree.insert(20);
-tree.BFS();
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
+console.log(tree.BFS());
