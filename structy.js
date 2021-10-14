3c99f5923eb633d1682fe9236bacc4f6187aa6f7
JosephASandoval <josephasandoval17@gmail.com>
2021-10-14 11:51:51 -0700

    updated

--- structy.js
+++ structy.js
@@ -439,3 +439,41 @@ const reverseList = (head, prev = null) => {
   // Time: O(n)
   // Space: O(n)
 };
+
+const zipperLists = (head1, head2) => {
+  // need two variables to keep track of the next node to add
+  // iterate thru both linked lists
+  // use a count variable to determine which linked list to add next
+  // add remainder of links if one list runs out
+  // return head1 at the end
+
+  let head = head1;
+  let tail = head;
+  let current1 = head1.next;
+  let current2 = head2;
+  let count = 0;
+
+  while (current1 !== null && current2 !== null) {
+    if (count % 2 === 0) {
+      tail.next = current2;
+      current2 = current2.next;
+    } else {
+      tail.next = current1;
+      current1 = current1.next;
+    }
+    tail = tail.next;
+    count++;
+  }
+
+  if (current1 !== null) tail.next = current1;
+  if (current2 !== null) tail.next = current2;
+
+  return head;
+
+  // n = length of list 1
+  // m = length of list 2
+  // Time: O(min(n, m))
+  // Space: O(1)
+};
+
+
