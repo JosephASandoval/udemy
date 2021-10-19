f95e95dc289b5646f1709ac27e710ce4ff7bb84b
JosephASandoval <josephasandoval17@gmail.com>
2021-10-19 13:39:14 -0700

    updated

---
+++ data_structures/hash_table.js
@@ -0,0 +1,52 @@
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+
+  _hash(key) {
+    // Given Hash func: 1) fast 2) uniformly distributes inputs 3)deterministic
+    let total = 0;
+    let WEIRD_PRIME = 31;
+
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+
+    return total;
+  }
+
+  set(key, value) {
+    let index = this._hash(key);
+
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+
+    this.keyMap[index].push([key, value]);
+  }
+
+  get(key) {
+    let index = this._hash(key);
+
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1];
+        }
+      }
+    }
+
+    return undefined;
+  }
+}
+
+let hashTable = new HashTable(17);
+hashTable.set("maroon", "#800000");
+hashTable.set("yellow", "#FFFF00");
+hashTable.set("olive", "#808000");
+hashTable.set("salmon", "#FA8072");
+hashTable.set("lightcoral", "#F08080");
+hashTable.set("mediumvioletred", "#C71585");
+hashTable.set("plum", "#DDA0DD");
