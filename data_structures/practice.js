99c66ad566834048abf8fea5726e0e717e6e1efd
JosephASandoval <josephasandoval17@gmail.com>
2021-10-19 16:01:35 -0700

    updated

--- data_structures/practice.js
+++
@@ -1,9 +0,0 @@
-const firstDuplicateValue = (array) => {
-  for (const value of array) {
-    const absValue = Math.abs(value);
-    const mappedIdx = absValue - 1
-    if (array[mappedIdx] < 0) return absValue;
-    array[mappedIdx] *= -1;
-  }
-  return -1;
-};
