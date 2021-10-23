acfe57a7cbe69223e12518082b4228bbfc15174e
JosephASandoval <josephasandoval17@gmail.com>
2021-10-23 12:53:14 -0700

    updated

--- algoexpert.js
+++ algoexpert.js
@@ -505,3 +505,18 @@ const removeDuplicatesFromLinkedList = (linkedList) => {
   // Time: O(n)
   // Space: O(1)
 };
+
+const productSum = (array, multiplier = 1) => {
+  let sum = 0;
+  for (const element of array) {
+    if (Array.isArray(element)) {
+      sum += productSum(element, multiplier + 1);
+    } else {
+      sum += element;
+    }
+  }
+  return sum * multiplier;
+
+  // Time: O(n)
+  // Space: O(1)
+};
