f86194bfd6d6fd34616f6447e74b706c0fdd89d7
JosephASandoval <josephasandoval17@gmail.com>
2023-05-25 17:59:53 -0700

    updated

---
+++ problem_solving_patterns.js
@@ -0,0 +1,13 @@
+// Frequency Counter: use a counter obj to store the count
+
+// Multiple Pointers: they refer to indicies so that you can move them; very useful if you have a sorted array
+
+// Sliding Window: 
+
+// Divide and Counquer
+
+// Dynamic Programming
+
+// Greedy Algorithms
+
+// Backtracking
